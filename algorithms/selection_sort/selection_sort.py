fd5783b7a281abc86702b69f72f8e7be8c4c6358
Alican Sungur <sunguralican@gmail.com>
2024-07-21 15:12:09 +0100

    add selection sort

---
+++ algorithms/selection_sort/selection_sort.py
@@ -0,0 +1,15 @@
+def selection_sort(arr):
+    size = len(arr)
+    # Use a new array to avoid mutation
+    sorted_arr = arr.copy() 
+    
+    for i in range(size):
+        lowest_num_idx = i
+        
+        for j in range(i + 1, size):
+            # Find and update the index of the smallest number.
+            if sorted_arr[j] < sorted_arr[lowest_num_idx]:
+                lowest_num_idx = j
+        # Swap numbers
+        sorted_arr[i], sorted_arr[lowest_num_idx] = sorted_arr[lowest_num_idx], sorted_arr[i]
+    return sorted_arr
