8cd624d2d1dcd328b5aa6c9a959c60945368e18e
Alican Sungur <sunguralican@gmail.com>
2024-09-17 11:01:10 +0100

    linked list implementation
    - Node, LL
    - Read
    - Search

---
+++ data_structures/linked_list/linked_list.py
@@ -0,0 +1,43 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next_node = None
+
+class LinkedList:
+    def __init__(self, first_node):
+        self.first_node = first_node
+
+    def read(self, index):
+        current_node = self.first_node
+        current_index = 0
+        while current_index < index:
+            current_node = current_node.next_node
+            current_index += 1
+            # Return None when out of bounds
+            if current_node is None:
+                return None
+        return current_node.data
+
+    def index_of(self, value):
+        current_node = self.first_node
+        current_index = 0
+        while current_node is not None:
+            if current_node.data == value:
+                return current_index
+
+            current_node = current_node.next_node
+            current_index += 1
+        # Return None if data is not found:
+        return None
+
+
+node_1 = Node("once")
+node_2 = Node("upon")
+node_3 = Node("a")
+node_4 = Node("time")
+node_1.next_node = node_2
+node_2.next_node = node_3
+node_3.next_node = node_4
+list = LinkedList(node_1)
+# print(list.read(2))
+print(list.index_of("baklava"))
