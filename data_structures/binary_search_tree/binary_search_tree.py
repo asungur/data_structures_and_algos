b1723f82131d8a81a2113d1b91b145bf45637b74
Alican Sungur <sunguralican@gmail.com>
2024-09-21 15:04:12 +0100

    insertion

--- data_structures/binary_search_tree/binary_search_tree.py
+++ data_structures/binary_search_tree/binary_search_tree.py
@@ -11,6 +11,18 @@ def search(searchValue, node):
         return search(searchValue, node.leftChild)
     else:
         return search(searchValue, node.rightChild)
+
+def insert(value, node):
+    if value < node.value:
+        if node.leftChild is None:
+            node.leftChild = TreeNode(value)
+        else:
+            insert(value, node.leftChild)
+    elif value > node.value:
+        if node.rightChild is None:
+            node.rightChild = TreeNode(value)
+        else:
+            insert(value, node.rightChild)
 node1 = TreeNode(25)
 node2 = TreeNode(75)
 root = TreeNode(50, node1, node2)
