268ab8ef123cd406263b580ad8aca8902810b7e4
Alican Sungur <sunguralican@gmail.com>
2024-09-21 15:04:21 +0100

    deletion

--- data_structures/binary_search_tree/binary_search_tree.py
+++ data_structures/binary_search_tree/binary_search_tree.py
@@ -23,6 +23,41 @@ def insert(value, node):
             node.rightChild = TreeNode(value)
         else:
             insert(value, node.rightChild)
+
+def delete(valueToDelete, node):
+    # Base case: you hit the bottom of the tree
+    if node is None:
+        return None
+
+    # Recursive calls
+    # Also return node to be used as the new value of its paren'ts left/right child:
+    elif valueToDelete < node.value:
+        node.leftChild = delete(valueToDelete, node.leftChild)
+        return node
+    elif valueToDelete > node.value:
+        node.rightChild = delete(valueToDelete, node.rightChild)
+        return node
+    elif valueToDelete == node.value:
+        # If no left or right child, return the other child. This way you delete the current node
+        if node.leftChild is None:
+            return node.rightChild
+        elif node.rightChild is None:
+            return node.leftChild
+        else:
+            # If it has two children, we use lift function to replace the node
+            node.rightChild = lift(node.rightChild, node)
+            return node
+
+def lift(node, nodeToDelete):
+    # Find a successor node if there is a left child:
+    if node.leftChild:
+        node.leftChild = lift(node.leftChild, nodeToDelete)
+        return node
+    else:
+        # If no leftChild, you reacheD to the successor node. Take its value and lift
+        nodeToDelete.value = node.value
+        return node.rightChild
+
 node1 = TreeNode(25)
 node2 = TreeNode(75)
 root = TreeNode(50, node1, node2)
