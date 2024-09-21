eea2def92991a92f01dec7d8e9e22622aa13d7de
Alican Sungur <sunguralican@gmail.com>
2024-09-21 15:04:03 +0100

    binary search tree

---
+++ data_structures/binary_search_tree/binary_search_tree.py
@@ -0,0 +1,16 @@
+class TreeNode:
+    def __init__(self, val, left=None, right=None):
+        self.value = val
+        self.leftChild = left
+        self.rightChild = right
+
+def search(searchValue, node):
+    if node is None or node.value == searchValue:
+        return node
+    elif searchValue < node.Value:
+        return search(searchValue, node.leftChild)
+    else:
+        return search(searchValue, node.rightChild)
+node1 = TreeNode(25)
+node2 = TreeNode(75)
+root = TreeNode(50, node1, node2)
