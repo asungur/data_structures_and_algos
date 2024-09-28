14829d1ebce8fed66bca51e6bb3c07e528753aa5
Alican Sungur <sunguralican@gmail.com>
2024-09-28 19:10:41 +0100

    insertion

--- data_structures/heap/heap.py
+++ data_structures/heap/heap.py
@@ -17,3 +17,18 @@ class Heap:
     def parent_index(self, initial_node_index):
         return (initial_node_index - 1) / 2
 
+    def insert(self, value):
+        # Turn the value into the last node by inserting at the end:
+        self.data.append(value)
+
+        # Fetch new node index:
+        new_node_index = len(self.data) - 1
+
+        # Trickle up algorithm:
+        # Iterate until the new node is either in root or it's lesser than its parent node:
+        while new_node_index > 0 and self.data[new_node_index] > self.data[self.parent_index(new_node_index)]:
+            # Swap new node with parent:
+            self.data[self.parent_index(new_node_index)], self.data[new_node_index] = self.data[new_node_index],  self.data[self.parent_index(new_node_index)]
+            # Update the node index:
+            new_node_index = self.parent_index(new_node_index)
+
